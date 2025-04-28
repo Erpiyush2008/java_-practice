5d3cc126a24a740e6a6b6dee23151b5ceeafd262
Erpiyush2008 <piyushsharma190708@gmail.com>
2025-04-28 22:44:46 +0530

    hello

---
+++ piyush.java
@@ -0,0 +1,10 @@
+public class piyush {
+    public static void main(String[] args) {
+        
+        int n = 5;
+        int result;
+        result = (n * (n + 1)) / 2;  // correct multiplication
+        System.out.println(result);
+
+    }
+}
